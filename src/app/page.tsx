141d15ca5c37041361ad245f1ef0001c33aa2975
Sentharion <michpawbert@gmail.com>
2026-05-11 16:22:39 +0200

    contactSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
-import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Services from "@/components/Services";
 import Portfolio from "@/components/Portfolio";
+import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
     <main className="min-h-screen relative bg-[#060613]">
       <div className="fixed inset-0 bg-linear-to-br from-[#250d5e] via-[#060613] to-[#100852] z-0" />
       <div className="relative z-10">
-        <Navbar />
         <Hero />
         <Services />
         <Portfolio />
+        <Contact />
       </div>
     </main>
   );
