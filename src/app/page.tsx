02f7e9fc5d688d89958bd6b60769f9a24b68547f
Sentharion <michpawbert@gmail.com>
2026-05-11 16:30:03 +0200

    footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Services from "@/components/Services";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
         <Navbar />
         <Hero />
         <Services />
+        <Footer />
       </div>
     </main>
   );
